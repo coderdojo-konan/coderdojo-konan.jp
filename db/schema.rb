2183709a9fbc9d6d3901989b09e690fe5a984685
Yuma Soga <ysoga19@gmail.com>
2018-08-27 18:43:59 +0900

    Add posts layout.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_22_104154) do
+ActiveRecord::Schema.define(version: 2018_08_27_093425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "body"
+    t.string "body"
     t.string "title"
     t.datetime "published_at"
     t.integer "author_id"
