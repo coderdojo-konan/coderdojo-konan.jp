84a676d7c245e3666b295b86fd9fae1d300c3c02
Yuma Soga <ysoga19@gmail.com>
2018-09-07 19:29:45 +0900

    #43 Add routes for Let's Encrypt.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/pages' => 'pages#index'
   get '/next-event-is-still-planned' => 'staticpages#next_event_is_still_planned'
   get '/ninjas-works' => 'staticpages#ninjas_works'
+  get ".well-known/acme-challenge/:id" => "staticpages#letsencrypt"
 
   resources :pages, only: [:show], :path => '/'
 
