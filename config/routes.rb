aa9a2b84c23254821650eadaac353ba35d396e22
Yuma Soga <ysoga19@gmail.com>
2018-12-21 22:03:41 +0900

    [Remove] Delete not exist routing

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,7 @@ Rails.application.routes.draw do
   get '/contact-form' => 'staticpages#contact'
 
   get '/pages' => 'pages#index'
-  get '/next-event-is-still-planned' => 'staticpages#next_event_is_still_planned'
-  get '/ninjas-works' => 'staticpages#ninjas_works'
+
 
   resources :pages, only: [:show], :path => '/'
 
