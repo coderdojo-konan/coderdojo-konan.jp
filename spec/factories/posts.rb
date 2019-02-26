cb966d489e095c22d2ac28caf82ddfa48f96fbf8
Yuma Soga <ysoga19@gmail.com>
2019-02-26 19:44:39 +0900

    Comment out factory of posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :post do
-    body "MyString"
-    title "MyString"
-    published_at "2018-08-27 18:30:48"
-    user_id 1
-    slug "MyString"
-    is_draft false
+    # body "MyString"
+    # title "MyString"
+    # published_at "2018-08-27 18:30:48"
+    # user_id 1
+    # slug "MyString"
+    # is_draft false
   end
 end
